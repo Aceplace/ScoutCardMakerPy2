62ca80755bc5e09e962b33e779a1dc7b19694c2b
Michael Yeiter <Aceplace1116@gmail.com>
2018-12-16 10:03:59 -0500

    Can add and remove conditions from a condition set.

--- defensiveformation/guis/conditionsetgui.py
+++ defensiveformation/guis/conditionsetgui.py
@@ -12,21 +12,28 @@ class ConditionSetGui(tk.Frame):
         condition_set_options_frame = tk.Frame(self)
         condition_set_options_frame.pack()
 
-        tk.Button(condition_set_options_frame, text='<--', command=self.raise_priority).pack(side=tk.LEFT)
-        tk.Button(condition_set_options_frame, text=' + ', command=self.add_condition_set).pack(side=tk.LEFT)
-        tk.Button(condition_set_options_frame, text=' - ', command=self.delete_condition_set).pack(side=tk.LEFT)
-        tk.Button(condition_set_options_frame, text='-->', command=self.lower_priority).pack(side=tk.LEFT)
+        tk.Button(condition_set_options_frame, text='Add', command=self.add_condition).pack(side=tk.LEFT)
+        tk.Button(condition_set_options_frame, text='Reset', command=self.reset).pack(side=tk.LEFT)
 
         # Widgets for modifying conditions
         self.conditions_frame = tk.Frame(self)
         self.conditions_frame.pack()
+        self.create_conditions_frame()
 
-        num_conditions_range = list(range(len(condition_set.conditions)))
-        conditions = self.condition_set.conditions
-        connectors = self.condition_set.connectors
-        connector_options = ['and','or']
+
+    def create_conditions_frame(self):
+        self.conditions_frame.pack_forget()
+        self.conditions_frame.destroy()
+        self.conditions_frame = tk.Frame(self)
+        self.conditions_frame.pack()
+
         self.connector_om_values = [None]
         self.condition_menu_values = []
+
+        num_conditions_range = list(range(len(self.condition_set.conditions)))
+        conditions = self.condition_set.conditions
+        connectors = self.condition_set.connectors
+        connector_options = ['and', 'or']
         for i, condition, connector in zip(num_conditions_range, conditions, connectors):
             if i != 0:
                 connector_value = tk.StringVar()
@@ -35,17 +42,14 @@ class ConditionSetGui(tk.Frame):
                                              command=self.condition_change)
                 connector_om.grid(row=i, column=0, sticky='WE')
                 self.connector_om_values.append(connector_value)
-                
+
             self.create_and_add_conditions_menu(i, condition)
-
-
-
 
     def create_and_add_conditions_menu(self, index, condition):
         self.condition_menu_values.append(tk.StringVar())
         condition_menu_value = self.condition_menu_values[index]
         condition_menu_value.set(condition)
-        menu_button = tk.Menubutton(self.conditions_frame, textvariable=condition_menu_value, indicatoron=True,
+        menu_button = tk.Menubutton(self.conditions_frame, textvariable=self.condition_menu_values[index], indicatoron=True,
                                     borderwidth=1, relief='raised')
         root_menu = tk.Menu(menu_button, tearoff=False)
         menu_button.configure(menu=root_menu)
@@ -79,19 +83,33 @@ class ConditionSetGui(tk.Frame):
 
 
     def condition_change(self, *args):
-        pass
+        self.update_defender()
+        self.create_conditions_frame()
 
-    def add_condition_set(self, *args):
-        pass
+    def update_defender(self):
+        conditions = [condition_menu_value.get() for condition_menu_value in self.condition_menu_values]
+        connectors = ['first' if connector_om_value == None else connector_om_value.get()
+                      for connector_om_value in self.connector_om_values]
+        for i in range(len(conditions)):
+            if conditions[i] == 'Delete Condition':
+                del conditions[i]
+                del connectors[i]
+                break
+        connectors[0] = 'first'
 
-    def raise_priority(self, *args):
-        pass
+        self.condition_set.conditions = conditions
+        self.condition_set.connectors = connectors
 
-    def lower_priority(self, *args):
-        pass
+    def add_condition(self, *args):
+        self.condition_set.conditions.append('Default')
+        self.condition_set.connectors.append('and')
+        self.create_conditions_frame()
 
-    def delete_condition_set(self, *args):
-        pass
+    def reset(self, *args):
+        self.condition_set.conditions = ['Default']
+        self.condition_set.connectors = ['First']
+        self.create_conditions_frame()
+
 
 
 
